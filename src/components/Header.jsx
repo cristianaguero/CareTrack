6ba293395898a007490b3d96da665781f40f23de
cristian <cristian.s.aguero@icloud.com>
2023-07-16 12:27:06 +0300

    project settings ready

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+function Header() {
+    return (
+        <header>
+            <h1 className='text-4xl font-bold'>Care<span className='text-indigo-600'>Track</span></h1>
+        </header>
+    )
+}
+
+export default Header
